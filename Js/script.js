a5c85d9e22d96b7af0cb8b3d6d588a5bc579bebd
ruru2 <106865021+ruru2@users.noreply.github.com>
2022-11-28 17:36:12 +0530

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+/*
+#sanjai R
+*/
+
+var mybutton = document.getElementById("home");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 40|| document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunc() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
